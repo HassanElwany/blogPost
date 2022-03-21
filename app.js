d2c19d8e1626c4048f1f1e90e51b17b9b47066d3
Hassan Elwany <elwany_hassan@hotmail.com>
2022-03-21 20:47:46 +0300

    involved mongoodb

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 // init express framework
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const { result } = require('lodash')
+
+
 
 // creating app using express
 const app = express()
-// register view emgine 
+
+//connecting mongodb databases 
+
+const dburl = `mongodb+srv://hassan07:hend1440@blogpost.bxn2o.mongodb.net/BlogPost?retryWrites=true&w=majority`
+mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true}).then ((result)=> app.listen(3000)).catch((err)=> console.log(err))
+// register view engine 
 app.set('view engine', 'ejs')
 
-app.listen(3000)
+
 
 
 app.use(express.static('public'))
+app.use(morgan('dev'))
 
+//routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Study', snippet: 'studding about node and fucking js :)'},
-        {title: 'Study', snippet: 'studding about node and fucking js :)'},
-        {title: 'Study', snippet: 'studding about node and fucking js :)'}
-    ]
-    res.render('index', { title: 'Home', blogs})
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
+})
+
+//blog route 
+app.get('/blogs', (req, res )=>{
+    Blog.find().sort({createdAt: -1})
+    .then((result)=>{
+       res.render('index', {title: 'All Blogs', blogs: result}) 
+    }).catch((err)=>{
+        console.log(err)
+    })
 })
 
 app.get('/blogs/create', (req, res) => {
